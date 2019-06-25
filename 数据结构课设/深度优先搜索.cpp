13e0e8276bb92979947fa15cf72eaaf7b9ee29c0
小游 <45655117+youlei55@users.noreply.github.com>
2019-06-25 17:16:09 +0800

    Update 深度优先搜索.cpp

--- 数据结构课设/深度优先搜索.cpp
+++ 数据结构课设/深度优先搜索.cpp
@@ -1,3 +1,50 @@
+/****************************88
+
+题目描述
+   输入一个图，用邻接矩阵存储（实际上也可以选择邻接表），并实现DFSTraverse操作。
+拷贝前面已经实现的代码，主函数必须如下，完成剩下的部分。 
+int main()
+{
+   Graph g;
+   CreateUDG(g);
+   DFSTraverse(g);
+   cout << endl;
+   DestroyUDG(g);
+   return 0;
+}//main 
+
+输入
+输入的第一行是两个整数，分别是图的总顶点数n和总边数e 
+第二行是n个空格分开的字符串，是顶点的名字，依次对应编号0~n-1。 
+随后有e行，每行两个空格分开的顶点名字，表示一条边的两个顶点。 
+具体见样例。 
+
+输出
+输出图的DFS序列，遍历次序按教材，每个顶点后面跟一个空格。 
+具体见样例。 
+
+样例输入
+8 9
+v1 v2 v3 v4 v5 v6 v7 v8
+v1 v2
+v1 v3
+v2 v4
+v2 v5
+v3 v6
+v3 v7
+v4 v8
+v5 v8
+v6 v7
+
+样例输出
+v1 v2 v4 v8 v5 v3 v6 v7 
+
+提示
+样例对应教材6.5的图G4（P161图6.17a）
+
+这里被坑了，输入的数据不一定是v开头的，也有可能是数字
+
+******************/
 #include<iostream>
 using namespace std;
 
@@ -12,20 +59,28 @@ typedef struct
 	int vexnum,arcnum;
 }Graph;
 
+int LocateVex(Graph G,char a[])
+{
+	int i;
+	for(i=0;i<G.vexnum;i++)
+		if(G.vexs[i][0]==a[0] &&  G.vexs[i][1]==a[1])
+			return i;
+	return -1;
+}
 void CreateUDG(Graph &G)
 {
 	int i,j,k;
 	char v1[3],v2[3];
 	cin>>G.vexnum>>G.arcnum;
 	for(i=0;i<G.vexnum;i++)
-		scanf("%s",&G.vexs[i]);
+		cin>>G.vexs[i];
 	for(i=0;i<G.vexnum;i++)
 		for(j=0;j<G.vexnum;j++)
 			G.arcs[i][j]=0;
 	for(k=0;k<G.arcnum;k++)
 	{
 		scanf("%s %s",&v1,&v2);
-		i=v1[1]-49;j=v2[1]-49;
+		i=LocateVex(G,v1);j=LocateVex(G,v2);
 		//printf("%d %d",i,j);
 		G.arcs[i][j]=1;
 		G.arcs[j][i]=1;
@@ -63,8 +118,12 @@ int NextAdjvex(Graph G,int v,int w)
 
 void DFSTraverse(Graph G,int v)
 {
-	int w;  
-	cout<<"v"<<v+1<<" ";
+	int w; 
+	if(G.vexs[v][1])
+		cout<<G.vexs[v][0]<<G.vexs[v][1]<<" ";
+	else
+		cout<<G.vexs[v][0]<<" ";
+	//printf("%s ",G.vexs[v]);
 	visited[v]=1;
 	for(w=FirstAdjvex(G,v);w>=0;w=NextAdjvex(G,v,w))
 		if(!visited[w]) DFSTraverse(G,w);
@@ -76,6 +135,15 @@ int main()
    CreateUDG(g);
    DFSTraverse(g,0);
    cout << endl;
-   //system("pause");
+   system("pause");
    return 0;
 }//main 
+
+
+
+
+
+
+
+
+
