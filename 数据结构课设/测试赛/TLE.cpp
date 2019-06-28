2b05db9a436643745b66ba61c6cdab8ba9ab6590
小游 <45655117+youlei55@users.noreply.github.com>
2019-06-28 10:35:05 +0800

    Create TLE.cpp

---
+++ 数据结构课设/测试赛/TLE.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+void process(char a[])
+{
+	
+}
+
+int main()
+{
+	int i,n,m;
+	char a[100];
+	cin>>n>>m;
+	for(i=0;i<m;i++)
+	{
+		scanf("%s",&a);
+		process(a);
+	}
+
+	system("pause");
+	return 0;
+}
