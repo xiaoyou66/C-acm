186deccb8d184caa6dda360974bab80824226f9e
小游 <45655117+youlei55@users.noreply.github.com>
2019-06-28 11:46:27 +0800

    Update TLE.cpp

--- 数据结构课设/测试赛/TLE.cpp
+++ 数据结构课设/测试赛/TLE.cpp
@@ -1,23 +1,53 @@
 #include<iostream>
+#include <math.h>
 using namespace std;
 
 
-void process(char a[])
+void process(char a[],double n)
 {
-	
+	int i;
+	double sum=1.0;
+	for(i=2;a[i];i++)
+	{
+		//printf("%c ",a[i]);
+		if(a[i]=='n' && a[i+2]=='n')
+		{
+			sum*=n*n;
+			i++;
+		}
+		if(a[i]=='n' && a[i+2]!='n')
+		{
+			sum*=n;
+			i++;
+		}
+		if(a[i]=='s')
+		{
+			sum*=sqrt(n);
+			i+=6;
+		}
+		if(a[i]=='l')
+		{
+			sum*=log(n);
+			i+=4;
+		}
+	}
+	if(sum>100000000)
+		printf("TLE\n");
+	else
+		printf("%.2lf\n",sum);
 }
 
 int main()
 {
-	int i,n,m;
+	int i,m;
+	double n;
 	char a[100];
 	cin>>n>>m;
 	for(i=0;i<m;i++)
 	{
 		scanf("%s",&a);
-		process(a);
+		process(a,n);
 	}
-
-	system("pause");
+	//system("pause");
 	return 0;
 }
